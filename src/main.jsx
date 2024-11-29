a1d2e8a01a346d53f90e4ece33d781643826fcc8
cbtruong <batruong12122002@gmail.com>
2024-11-30 02:04:04 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import Quizizz from './pages/Quizziz/Quizizz.jsx';
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ThemeProvider>
-			<App />
+			<Quizizz />
 		</ThemeProvider>
 	</React.StrictMode>
 );
