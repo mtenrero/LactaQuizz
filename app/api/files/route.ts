9f6534f18a7c688147092285716f324329f3cb62
Marcos Tenrero <mtenrero@allot.com>
2025-01-04 02:00:01 +0100

    keep ordering topics by date
    Signed-off-by: Marcos Tenrero <mtenrero@allot.com>

--- app/api/files/route.ts
+++ app/api/files/route.ts
@@ -12,7 +12,15 @@ export async function GET(req: NextRequest) {
             select: { source_file: true }
         });
 
-        const options = filenames.map(file => file.source_file);
+        const options = filenames
+            .map(file => file.source_file)
+            .sort((a, b) => {
+                const matchA = a.match(/(\d{4}-\d{2}-\d{2})/);
+                const matchB = b.match(/(\d{4}-\d{2}-\d{2})/);
+                const dateA = new Date(matchA ? matchA[0] : '');
+                const dateB = new Date(matchB ? matchB[0] : '');
+            return dateA.getTime() - dateB.getTime();
+            });
         return NextResponse.json(options);
     } catch (err) {
         console.log(err);
